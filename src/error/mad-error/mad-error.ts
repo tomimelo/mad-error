8d7a816dc5b09be4fdf6e23492f13175e8ec090b
Tomas Melone <tomasmelone@hotmail.com>
2022-08-19 20:07:58 +0200

    create mad error

---
+++ src/error/mad-error/mad-error.ts
@@ -0,0 +1,40 @@
+import { CustomError } from '../custom-error'
+
+export interface StringifiedMadError {
+  name: string,
+  message: string,
+  status: number,
+  code?: string
+}
+
+export class MadError extends Error implements CustomError {
+  private statusCode: number = 500
+  private errorCode: string | undefined
+  public constructor (message: string) {
+    super(message)
+    this.name = 'MadError'
+  }
+
+  public status (statusCode: number): MadError {
+    this.statusCode = statusCode
+    return this
+  }
+
+  public code (errorCode: string): MadError {
+    this.errorCode = errorCode
+    return this
+  }
+
+  public toJSON (): StringifiedMadError {
+    return {
+      name: this.name,
+      message: this.message,
+      status: this.statusCode,
+      ...(!this.isNullish(this.errorCode) && { code: this.errorCode })
+    }
+  }
+
+  private isNullish (value: any): boolean {
+    return value === undefined || value === null
+  }
+}
